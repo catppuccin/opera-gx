ba4d8231c09ee7c4031937839a345866da9d37b0
isabel <isabel@isabelroses.com>
2023-04-29 14:28:49 +0100

    yes

---
+++ generate.py
@@ -0,0 +1,48 @@
+from dataclasses import fields
+from pathlib import Path
+from string import Template
+
+from catppuccin import Flavour
+
+OUT_DIR = Path("build")
+OUT_DIR.mkdir(exist_ok=True)
+TEMPLATE_STR = Path("src/template.json").read_text()
+
+themes = {
+    "latte": Flavour.latte(),
+    "mocha": Flavour.mocha(),
+    "macchiato": Flavour.macchiato(),
+    "frappe": Flavour.frappe(),
+}
+
+accents = [
+    "Rosewater",
+    "Flamingo",
+    "Pink",
+    "Mauve",
+    "Red",
+    "Maroon",
+    "Peach",
+    "Yellow",
+    "Green",
+    "Teal",
+    "Sky",
+    "Sapphire",
+    "Blue",
+    "Lavender",
+]
+
+def palette():
+    return {
+        flavour_method.__name__: {
+            field.name: f"#{getattr(flavour_method(), field.name).hex}"
+            for field in fields(flavour_method())
+        } for flavour_method in themes
+    }
+
+
+if __name__ == "__main__":
+    template = Template(TEMPLATE_STR)
+    for flavour, colours in palette().items():
+        substituted_template = template.substitute(colours)
+        open(OUT_DIR / f"{flavour}/mainfest.json", 'w').write(substituted_template)
