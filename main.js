9fcb5186fa158e52cb26119329b897429ef78a78
isabel <isabel@isabelroses.com>
2023-04-29 16:57:44 +0100

    idek

---
+++ main.js
@@ -0,0 +1,81 @@
+import { variants } from "@catppuccin/palette";
+
+const accents = [
+  "rosewater",
+  "flamingo",
+  "pink",
+  "mauve",
+  "red",
+  "maroon",
+  "peach",
+  "yellow",
+  "green",
+  "teal",
+  "sky",
+  "sapphire",
+  "blue",
+  "lavender",
+];
+
+function hexToHSL(H) {
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return { h, s, l };
+}
+
+(async () => {
+  for (const flavour of Object.keys(variants)) {
+    const lib = {};
+
+    Object.keys(variants[flavour]).forEach((colour) => {
+      const hex = variants[flavour][colour]["hex"];
+      const { h, s, l } = hexToHSL(hex);
+      lib[colour] = {
+        h,
+        s,
+        l,
+      };
+    });
+    for (const accent of accents) {
+      lib.accent = lib[accent];
+
+    }
+  }
+})();
